c2d191f855ae09a0ba1f42d879fee78a7387c9e7
Your Name <uarealtek1994@gmail.com>
2024-09-08 22:57:10 +0300

    some dummy things

--- Main.py
+++ Main.py
@@ -1,4 +1,11 @@
-name = "Volodymyr"
-for i in name:
-    print(i)
+def reverse(lst):
+    index = len(lst) -1
+    out = list()
 
+    while index >= 0:
+        out.append(lst[index])
+        index -= 1
+    return out
+
+test = [1,2,3]
+print(reverse(test))
